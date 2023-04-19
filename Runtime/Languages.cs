3c3492b124974448f36b24042830de166d7629e5
Maxi Muller <maximuller11@gmail.com>
2023-04-19 15:23:56 -0300

    Languages enum expanded

--- Runtime/Languages.cs
+++ Runtime/Languages.cs
@@ -2,14 +2,36 @@ namespace Padoru.Localization
 {
 	public enum Languages
 	{
-		EN = 0,
-		ES = 1,
-		DE = 2,
-		FR = 3,
-		IT = 4,
-		JA = 5,
-		PT = 6,
-		RU = 7,
-		CN = 8
+		en_US = 0, // English
+		es_ES = 1, // Spanish
+		de_DE = 2, // German
+		fr_FR = 3, // French
+		it_IT = 4, // Italian
+		ja_JP = 5, // Japanese
+		pt_PT = 6, // Portuguese
+		ru_RU = 7, // Russian
+		zh_CN = 8, // Chinese
+		id_ID = 9, // Indonesian
+		ur_PK = 10, // Urdu
+		hi_IN = 11, // Hindi
+		ar_SA = 12, // Arabic
+		pa_IN = 13, // Panjabi; Punjabi
+		jv_ID = 14, // Javanese
+		te_IN = 15, // Telugu
+		mr_IN = 16, // Marathi
+		ta_IN = 17, // Tamil
+		vi_VN = 18, // Vietnamese
+		ko_KR = 19, // Korean
+		bn_BD = 20, // Bengali
+		pl_PL = 21, // Polish
+		tr_TR = 22, // Turkish
+		fa_IR = 23, // Persian
+		uk_UA = 24, // Ukrainian
+		ro_RO = 25, // Romanian; Moldavian; Moldovan
+		nl_NL = 26, // Dutch; Flemish
+		hu_HU = 27, // Hungarian
+		sv_SE = 28, // Swedish
+		fi_FI = 29, // Finnish
+		el_GR = 30, // Greek (modern)
 	}
 }
