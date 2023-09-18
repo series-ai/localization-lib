5786ff901032cf494d3e17591cfcaccde8436fa0
Maxi Muller <maximuller11@gmail.com>
2023-09-18 09:45:20 -0300

    Preserve attribute added to LocalizationFile constructor to stop unity from stripping it out

--- Runtime/LocalizationFile.cs
+++ Runtime/LocalizationFile.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEngine.Scripting;
 
 namespace Padoru.Localization
 {
@@ -11,5 +12,13 @@ namespace Padoru.Localization
 		public string FileName => fileName;
 		
 		public Dictionary<string, Dictionary<Languages, string>> Entries => entries;
+
+		// We need a constructor with Preserve attribute so unity doesn't strip it out.
+		// Stripping it will cause runtime errors when the game tries to load the localization file. 
+		[Preserve]
+		public LocalizationFile()
+		{
+			
+		}
 	}
 }
